a827a675e6c5f35739ac4343cf636a8932cfc3bf
johnmetzbower <johnmetzbower@nokware.net>
2015-02-03 19:57:04 -0600

    Method Overloading

--- WebCasts/Program.cs
+++ WebCasts/Program.cs
@@ -8,8 +8,28 @@ namespace WebCasts
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            Console.WriteLine("sume of two ints is {0}", sum(45, 65));
+            Console.WriteLine("sume of two doubles is {0}", sum(45.5, 64.5));
+            Console.WriteLine("sume of two strings is {0}", sum("Hello ", "World"));
+            Console.ReadKey();
+
+        }
+        public static int sum(int x, int y)
+        {
+            int add = x + y;
+            return add;
+        }
+        public static double sum(double x, double y)
+        {
+            double add = x + y;
+            return add;
+        }
+        public static string sum(string x, string y)
+        {
+            string add = x + y;
+            return add;
         }
     }
 }
