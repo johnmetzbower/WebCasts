4c0a9edc66fc52a33beeef1f3623282d3b117dd5
johnmetzbower <johnmetzbower@nokware.net>
2015-02-03 20:14:46 -0600

    Classes and Object

--- WebCasts/Program.cs
+++ WebCasts/Program.cs
@@ -6,10 +6,27 @@ using System.Threading.Tasks;
 
 namespace WebCasts
 {
+    class Box
+    {
+        public double height, length, width;
+
+        public double volume()
+        {
+            return (this.height * this.length * this.width);
+        }
+    }
     class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            Box boxOne = new Box();
+            boxOne.length = 10;
+            boxOne.height = 10;
+            boxOne.width = 10;
+
+            Console.WriteLine("volume of boxOne is {0}", boxOne.volume());
+
+            Console.ReadKey();
         }
     }
 }
