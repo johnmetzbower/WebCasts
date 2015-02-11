473ea2ea095ef31a4b47d26d3684cb55eff53788
johnmetzbower <johnmetzbower@nokware.net>
2015-02-10 18:25:02 -0600

    Constructors and Destructors

--- WebCasts/Program.cs
+++ WebCasts/Program.cs
@@ -14,19 +14,30 @@ namespace WebCasts
         {
             return (this.height * this.length * this.width);
         }
+        public Box(double h, double l, double w)
+        {
+            this.height = h;
+            this.length = l;
+            this.width = w;
+            Console.WriteLine("the constuctor is instantiated");
+        }
+        ~Box()
+        {
+            Console.WriteLine("The destructor is called");
+        }
     }
     class Program
     {
         public static void Main()
         {
-            Box boxOne = new Box();
-            boxOne.length = 10;
+            Box boxOne = new Box(10, 10, 10);
+            /*boxOne.length = 10;
             boxOne.height = 10;
-            boxOne.width = 10;
+            boxOne.width = 10;*/
 
             Console.WriteLine("volume of boxOne is {0}", boxOne.volume());
 
-            Console.ReadKey();
+            //Console.ReadKey();
         }
     }
 }
