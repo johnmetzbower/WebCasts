08730c6b243fc397b6c531878cbf18c97bbba46d
johnmetzbower <johnmetzbower@nokware.net>
2015-02-16 19:34:05 -0600

    Getter Setter
    Feb 16, 2015

--- WebCasts/Program.cs
+++ WebCasts/Program.cs
@@ -6,10 +6,48 @@ using System.Threading.Tasks;
 
 namespace WebCasts
 {
-    class Program
+    class Book
     {
-        static void Main(string[] args)
+        private int _id;
+        private string _bookName;
+        private int _noOfPages = 250;
+        
+        public void SetId(int Id)
         {
+            if (Id < 0)
+                throw new Exception("The book ID is not valid");
+            this._id = Id;
+        }
+        public int GetId(int Id)
+        {
+            return this._id;
+        }
+        public void SetBookName(string Bookname)
+        {
+            if (string.IsNullOrEmpty(Bookname))
+                throw new Exception("The book name is not valid");
+            this._bookName = Bookname;
+        }
+        public int GetBookName(String Bookname)
+        {
+            return this._id;
+        }
+    }
+
+    class ExampleOne
+    {
+        public static void Main()
+        {
+            Book B = new Book();
+            B.SetId(10);
+            B.SetBookName(null);
+            
+            Console.WriteLine("Book ID is {0}", B.SetId());
+            Console.WriteLine("Book name is {0}", B.SetBookName());
+
+           
+
+            Console.ReadKey();
         }
     }
 }
