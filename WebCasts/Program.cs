cbbed6268fd392e004d48a72b235e4c2617c96f7
johnmetzbower <johnmetzbower@nokware.net>
2015-03-02 20:00:48 -0600

    Back to Master - fix

--- WebCasts/Program.cs
+++ WebCasts/Program.cs
@@ -6,34 +6,13 @@ using System.Threading.Tasks;
 
 namespace WebCasts
 {
-    interface IMeter1
+    class ExampleONe
     {
-        void print();
-    }
-    interface IMeter2
-    {
-        void print();
-    }
-
-    class Program : IMeter1, IMeter2 
-    {
-        void IMeter1.print()
-        {
-            Console.WriteLine("Print Meter 1");
-        }
-        void IMeter2.print()
-        {
-            Console.WriteLine("Print Meter 2");
-        }
-
 
         public static void Main()
         {
-            Program ex = new Program();
-            ((IMeter1)ex).print();
-            ((IMeter2)ex).print();
 
-            Console.ReadKey();
         }
+
     }
 }
