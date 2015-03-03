374e4d823886f5ccfbf34e4502e9f68e0d2ae698
johnmetzbower <johnmetzbower@nokware.net>
2015-03-02 20:13:01 -0600

    Abstract Classes

--- WebCasts/Program.cs
+++ WebCasts/Program.cs
@@ -6,12 +6,30 @@ using System.Threading.Tasks;
 
 namespace WebCasts
 {
-    class ExampleONe
+    public abstract class Meter
     {
+        public abstract void printMeter1();
+        public void printMeter2()
+        {
+            Console.WriteLine("We are in printMeter2");
+        }
+    }
 
+    class ExampleOne : Meter
+    {
+        public override void printMeter1()
+        {
+            Console.WriteLine("We are in printMeter1");
+        }
         public static void Main()
         {
 
+            ExampleOne ex1 = new ExampleOne();
+            ex1.printMeter1();
+            ex1.printMeter2();
+
+
+            Console.ReadKey();
         }
 
     }
