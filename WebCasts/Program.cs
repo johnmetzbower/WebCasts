f69a311860831491a0c0f2e1fa510a68074ebac3
johnmetzbower <johnmetzbower@nokware.net>
2015-03-02 19:56:47 -0600

    C# 24 Explicit Interface

--- WebCasts/Program.cs
+++ WebCasts/Program.cs
@@ -6,10 +6,34 @@ using System.Threading.Tasks;
 
 namespace WebCasts
 {
-    class Program
+    interface IMeter1
     {
-        static void Main(string[] args)
+        void print();
+    }
+    interface IMeter2
+    {
+        void print();
+    }
+
+    class Program : IMeter1, IMeter2 
+    {
+        void IMeter1.print()
         {
+            Console.WriteLine("Print Meter 1");
+        }
+        void IMeter2.print()
+        {
+            Console.WriteLine("Print Meter 2");
+        }
+
+
+        public static void Main()
+        {
+            Program ex = new Program();
+            ((IMeter1)ex).print();
+            ((IMeter2)ex).print();
+
+            Console.ReadKey();
         }
     }
 }
