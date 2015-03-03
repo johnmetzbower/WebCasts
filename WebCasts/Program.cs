0c40bff19f033977f8cb6929ca09e6b3d852e9a2
johnmetzbower <johnmetzbower@nokware.net>
2015-03-02 20:33:07 -0600

    C# 27 Delegates

--- WebCasts/Program.cs
+++ WebCasts/Program.cs
@@ -6,12 +6,23 @@ using System.Threading.Tasks;
 
 namespace WebCasts
 {
-    class ExampleONe
+    public delegate int addNumbersDelegate(int number1, int number2);
+    class ExampleOne
     {
+        public static int addNumbers(int number1, int number2)
+        {
+
+            return number1 + number2;
+        }
+
 
         public static void Main()
         {
+            addNumbersDelegate del = new addNumbersDelegate(addNumbers);
+            int result = del(10, 10);
+            Console.WriteLine(result);
 
+            Console.ReadKey();
         }
 
     }
