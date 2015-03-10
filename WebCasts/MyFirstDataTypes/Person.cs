4d1dd8e4087d477a57e3642899aa59e5a5be59e4
johnmetzbower <johnmetzbower@nokware.net>
2015-03-09 21:30:27 -0500

    Bool and Switch Statements
    Learn C# Lesson 13

--- WebCasts/MyFirstDataTypes/Person.cs
+++ WebCasts/MyFirstDataTypes/Person.cs
@@ -10,14 +10,13 @@ namespace MyFirstProject.MyFirstDataTypes
     {
         public string FirstName { get; private set;}
         public string LastName { get; private set;}
-        public string MiddleName { get; private set; }
 
 
-        public Person(string firstName, string middleName, string lastName)
+        public Person(string firstName, string lastName = "")//makes last name optional
         {
             FirstName = firstName;
             LastName = lastName;
-            MiddleName = middleName;
+         
         }
         public string SayHello(string name)
         {
--- WebCasts/Program.cs
+++ WebCasts/Program.cs
@@ -12,13 +12,74 @@ namespace MyFirstProject
 
         static void Main(string[] args)
         {
-            Person john = new Person("John", "David", "Metzbower"), aline = new Person("Aline", "Metzbower", "Gonzalez");
 
-            string greeting = john.SayHello(aline);
-
-            Console.WriteLine(greeting);
-
+            #region Old Class Related Code
             
+           
+            //Person john = new Person("John", "David", "Metzbower"), aline = new Person("Aline", "Metzbower", "Gonzalez");
+
+            //string greeting = john.SayHello(aline);
+
+            //Console.WriteLine(greeting);
+            #endregion
+
+
+            Person bob = new Person("Bob", "Bob"), bob2 = new Person("Bob", "Bob");
+            
+            //Logical AND &&
+            //true && ture - true
+            //true && false - false
+            //fasle && true - false (short circuit)
+            // false && false - false
+
+            // logical OR ||
+            // true || true - true
+            // true || false - true (short circuit)
+            // false || true - true
+            // false || false - false
+
+            // logical NOT !
+            // !true - false
+            // !false - true
+
+            string str = "test";
+
+            /*if (str == "test")
+            {
+                Console.WriteLine("this is true");    
+            }
+            else if (str == "Test")
+            {
+                Console.WriteLine("this is also true");
+            }
+            else
+            {
+                Console.WriteLine("this is false");
+            }*/
+            
+            switch (str)
+            {
+                case "test":
+                    Console.WriteLine(@"the value is ""test""");
+                    break;
+                case "Test":
+                    Console.WriteLine(@"the value is ""Test""");
+                    break;
+                case "tEst":
+                    Console.WriteLine(@"the value is ""tEst""");
+                    break;
+                case "tesT":
+                    Console.WriteLine(@"the value is ""tesT""");
+                    break;
+                default:
+                    Console.WriteLine("no value was found");
+                        break;
+            }
+            
+            
+            
+            
+            Console.WriteLine("\nWe're out of the switch");
 
         }
     }
